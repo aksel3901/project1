e56feb66a17ae9ef434e1ba85b894ba67d02ab87
Juarez <juarezaksel@gmail.com>
2020-02-07 13:54:48 -0600

    added testproject models

--- testproject/noble_tutoring/models.py
+++ testproject/noble_tutoring/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class StudentUser(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    student_id = models.PositiveIntegerField(primary_key=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    grad_year = models.IntegerField()
+    
+class Campus(models.Model):
+    pass
+class Course(models.Model):
+    pass
