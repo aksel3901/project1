fa1745d2ebca6fb60af1b6ab36fb100db623a3b9
Juarez <juarezaksel@gmail.com>
2020-02-14 11:46:50 -0600

    added changes to models

--- testproject/noble_tutoring/models.py
+++ testproject/noble_tutoring/models.py
@@ -7,8 +7,14 @@ class StudentUser(models.Model):
     student_id = models.PositiveIntegerField(primary_key=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     grad_year = models.IntegerField()
-    
+    campus = models.ForeignKey('Campus', on_delete=models.CASCADE)
+
 class Campus(models.Model):
-    pass
+    name = models.CharField()
+
+    # logo_picture =
+
 class Course(models.Model):
+    name = models.CharField()
+
     pass
