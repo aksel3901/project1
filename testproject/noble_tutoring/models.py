3a493a93ddeeca117b581a6a92ffd8af1981b91b
Juarez <juarezaksel@gmail.com>
2020-02-14 14:06:26 -0600

    added required length fields and relationships

--- testproject/noble_tutoring/models.py
+++ testproject/noble_tutoring/models.py
@@ -8,13 +8,14 @@ class StudentUser(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     grad_year = models.IntegerField()
     campus = models.ForeignKey('Campus', on_delete=models.CASCADE)
+    email = models.EmailField()
 
 class Campus(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
+    courses = models.ManyToManyField('Course')
 
     # logo_picture =
 
 class Course(models.Model):
-    name = models.CharField()
-
-    pass
+    name = models.CharField(max_length=50)
+    students = models.ManyToManyField('StudentUser')
