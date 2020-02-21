1f1e4140f27d7623cb960ffb80a7fb8b4819086b
Juarez <juarezaksel@gmail.com>
2020-02-21 11:47:51 -0600

    added models into admin page

--- testproject/noble_tutoring/admin.py
+++ testproject/noble_tutoring/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from noble_tutoring.models import StudentUser, Campus, Course
 
 # Register your models here.
+admin.site.register(StudentUser)
+admin.site.register(Campus)
+admin.site.register(Course)
