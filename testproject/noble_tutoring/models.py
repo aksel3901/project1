6e38a0a9847a3a8cc6c82114bc333ef1137960b0
Juarez <juarezaksel@gmail.com>
2020-02-21 11:49:28 -0600

    added changes to models with string function

--- testproject/noble_tutoring/models.py
+++ testproject/noble_tutoring/models.py
@@ -10,12 +10,23 @@ class StudentUser(models.Model):
     campus = models.ForeignKey('Campus', on_delete=models.CASCADE)
     email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
 class Campus(models.Model):
     name = models.CharField(max_length=50)
     courses = models.ManyToManyField('Course')
 
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = 'campuses'
     # logo_picture =
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
     students = models.ManyToManyField('StudentUser')
+
+    def __str__(self):
+        return f"{self.name}"
