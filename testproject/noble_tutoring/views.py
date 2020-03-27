9d6bb935aea79a49ffe248be246885d44fa0ef57
Aksel <aksel.juarez@speeracademy.org>
2020-03-27 12:51:53 -0500

    addded render shortcut to views

--- testproject/noble_tutoring/views.py
+++ testproject/noble_tutoring/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse 
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Course
 
 def index(request):
     all_courses = Course.objects.all()
-    template = loader.get_template('noble_tutoring/index.html')
     context = {'all_courses' : all_courses, }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'noble_tutoring/index.html', context)
