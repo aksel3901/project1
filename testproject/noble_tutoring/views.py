0d336a67da9ac5566fa35aad5507149d3b384e3b
Aksel <aksel.juarez@speeracademy.org>
2020-03-27 12:36:25 -0500

    add courses list path to views

--- testproject/noble_tutoring/views.py
+++ testproject/noble_tutoring/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse 
+from django.template import loader
+
+from .models import Course
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Noble Tutoring index.")
+    all_courses = Course.objects.all()
+    template = loader.get_template('noble_tutoring/index.html')
+    context = {'all_courses' : all_courses, }
+    return HttpResponse(template.render(context, request))
