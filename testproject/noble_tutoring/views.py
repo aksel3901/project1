45cfc89864486c562a9b0949670e036815bf9123
Aksel <aksel.juarez@speeracademy.org>
2020-04-03 12:35:11 -0500

    changed detail to respond with 404 error

--- testproject/noble_tutoring/views.py
+++ testproject/noble_tutoring/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.shortcuts import get_object_or_404
+# from django.http import HttpResponse
 
 from .models import Course
 
@@ -9,4 +11,7 @@ def index(request):
     return render(request, 'noble_tutoring/index.html', context)
 
 def detail(request, course_id):
-    return HttpResponse("You're looking at course %s" % course_id)
+    course = get_object_or_404(Course, pk=course_id)
+    context = {'course' : course, }
+    return render(request, 'noble_tutoring/course.html', context)
+    # return HttpResponse("You're looking at course %s" % course_id)
