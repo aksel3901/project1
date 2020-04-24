0155bff38010971dbd8712ca99e2dbdbe697bda6
Aksel <aksel.juarez@speeracademy.org>
2020-04-24 12:22:13 -0500

    added path to import homepage views

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from noble_tutoring.views import homepage_view
 
 urlpatterns = [
     path('tutoring/', include('noble_tutoring.urls')),
     path('admin/', admin.site.urls),
+    path('', homepage_view)
 ]
