36eff85cd510ba82386c1ad53abf6d113ad7ed50
JuarezChris <42192084+JuarezChris@users.noreply.github.com>
2020-05-11 22:11:36 -0500

    added 4 new paths between pages
    Its pretty cold not gonna lie

--- testproject/noble_tutoring/urls.py
+++ testproject/noble_tutoring/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('courses/<int:course_id>/', views.detail, name='rooms')
+    path('courses/<int:course_id>/', views.detail, name='rooms'),
+    path('login_page', views.login_page),
+    path('login_user', views.login_user),
+    path('register_page', views.register_page),
+    path('profile_page', views.profile_page)
 ]
