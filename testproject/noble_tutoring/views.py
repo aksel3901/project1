2bb844e36f6bbd27d1432c6a74fd436853b813d6
Aksel <aksel.juarez@speeracademy.org>
2020-05-15 16:08:31 -0500

    updated

--- testproject/noble_tutoring/views.py
+++ testproject/noble_tutoring/views.py
@@ -4,10 +4,6 @@ from django.shortcuts import get_object_or_404
 # from django.http import HttpResponse
 
 from .models import *
-<<<<<<< HEAD
-=======
-
->>>>>>> 592fd72a14edeb1cfb8aec21ffc598a4d08cb154
 
 def index(request):
     all_courses = Course.objects.all()
@@ -25,32 +21,16 @@ def detail(request, course_id):
 def homepage_view(request):
     return render(request, 'noble_tutoring/homepage.html')
 
-<<<<<<< HEAD
 def login_page(request):
     return render(request, 'noble_tutoring/login_page.html')
 
 def register_page(request):
     return render(request, 'noble_tutoring/register_page.html')
 
-=======
-
-def login_page(request):
-    return render(request, 'noble_tutoring/login_page.html')
-
-
-def register_page(request):
-    return render(request, 'noble_tutoring/register_page.html')
-
-
->>>>>>> 592fd72a14edeb1cfb8aec21ffc598a4d08cb154
 def login_user(request):
     if request.method == "POST":
         user = StudentUser.objects.get(email=request.POST['email'])
     return redirect("/tutoring/profile_page")
 
 def profile_page(request):
-<<<<<<< HEAD
     return render(request, 'noble_tutoring/profile_page.html')
-=======
-    return render(request, 'noble_tutoring/profile_page.html')
->>>>>>> 592fd72a14edeb1cfb8aec21ffc598a4d08cb154
